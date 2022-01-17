faea6ca675b30ee25dae92e76849db721130b5fb
Macsono <97427571+Macsono@users.noreply.github.com>
2022-01-17 17:52:49 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,11 +6,17 @@
 #This function takes in a matrix 'x', initializes the inverse to
 #null to wipe out any inverse matrix from previous computations. Within this function, we have 4 nested functions,
 #set() helps to transfer any input matrix we wish to find its inverse say (y) to the parent environment, in a 
-#sense mutating it to x.while maintaining the null value of the inverse.
-#get() retrieves the 'new' x matrix.
+#sense mutating it to x.while maintaining the null value of the inverse. The super operator <<- helps to do this
+#transformation of y from the local environment to the argument x in the parent environment which was an argument
+#in the parent function.
+#get() retrieves the 'new' x matrix. Uses lexical scoping to retrieve x though x is not defined within this function
 #setinverse() stores the newly solved inverse into i in the parent environment
-#getinverse() returns the cached inverse
-#A list is returned which contains these four functions
+#getinverse() returns the cached inverse. Uses lexical scoping to retrieve i even though we did not define i within 
+#this function
+#A list is returned which contains these four functions.
+#The object myMatrix is a special object of type makeCacheMatrix which contains the list of functions returned from 
+#makeCacheMatrix, but also retains the entire environment of makeCacheMatrix including all arguments initially passed
+#into makeCacheMatrix. This is unique about lexical scoping, especially when functions are returned from a function execution
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
